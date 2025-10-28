a79f4e87c9aceca0d7c0b1c76219cce2b9682345
adhamady <adhamamrkamalmady@gmail.com>
2025-10-28 15:04:53 +0300

    ADD: strings.jl

---
+++ variables/strings.jl
@@ -0,0 +1,11 @@
+my_char = 'x'
+println(Int(my_char))
+println(Char(120)) 
+
+
+my_str = "this is a string using julia"
+println("my_str is: " * my_str)
+println("the length of my_str is $(length(my_str))")
+println("first char of my string is:  $(my_str[1])")
+println("the last char in my string is: $(my_str[end]) ")
+println("the first 7 chars in my str are: $(my_str[1:7])")
